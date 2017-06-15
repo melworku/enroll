ab09afce914b211b80b1737106c1f1924159bce2
harsha280 <harsha.vskr@gmail.com>
2017-06-15 10:07:10 -0400

    Removed Default DCHL, Link To employer

--- app/models/document.rb
+++ app/models/document.rb
@@ -13,7 +13,7 @@ class Document
   field :title, type: String, default: "untitled"
 
   # Entity responsible for making the resource - person, organization or service
-  field :creator, type: String, default: "dchl"
+  field :creator, type: String
 
   # Controlled vocabulary w/classification codes. Mapped to ConsumerRole::VLP_DOCUMENT_KINDS
   field :subject, type: String
